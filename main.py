e4b33f7e229c5b34935d72484a62afcb0abf14c8
agwstu <aa.wytrykowska@student.uw.edu.pl>
2021-11-24 18:53:36 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+import scipy
+
+from scipy.io import arff
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+wd ='/Users/agatawytrykowska/Desktop/studia/magisterka/3sem/Analiza wielowymiarowa/data'
+
+data = arff.loadarff(wd+'/'+'1year.arff')
+df = pd.DataFrame(data[0])
+print(df.head())
